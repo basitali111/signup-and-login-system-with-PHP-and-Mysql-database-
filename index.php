df6e2987e351ecece7b603dd960a3f8adf0d8e04
basitali111 <allahbadshah148@gmail.com>
2023-04-11 00:39:28 +0500

    Adding setup files

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+$_SESSION;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My website</title>
+</head>
+<body>
+    <a href="logout.php">logout</a>
+    <h1>This is an index page</h1>
+    <br>
+    Hello,username
+</body>
+</html>
