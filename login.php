80c5d3de646c0ff046ba2c376f33494a548ca3d0
basitali111 <allahbadshah148@gmail.com>
2023-04-14 23:53:02 +0500

    Add login code

--- login.php
+++ login.php
@@ -10,6 +10,15 @@
     <title>Login</title>
 </head>
 <body>
-    
+<h1>Login Form</h1>
+    <form>
+      <label for="username">Username:</label>
+      <input type="text" id="username" name="username" required><br>
+
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password" required><br>
+
+      <input type="submit" value="Submit">
+    </form>
 </body>
 </html>
