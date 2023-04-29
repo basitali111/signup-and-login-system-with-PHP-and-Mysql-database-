92e7fb2aa88d379d95333c82b93da6a3cd32c4a3
basitali111 <allahbadshah148@gmail.com>
2023-04-29 23:27:34 +0500

    adding tailwalind css setup

--- login.php
+++ login.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link href="styles.css" rel="stylesheet">
 </head>
 <body>
 <h1>Login Form</h1>
@@ -17,7 +18,7 @@
 
       <label for="password">Password:</label>
       <input type="password" id="password" name="password" required><br>
-
+    
       <input type="submit" value="Submit">
     </form>
 </body>
