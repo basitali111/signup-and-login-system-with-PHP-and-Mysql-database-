bb4ee5c1e83c6faae8dd978e6fe01269c54bda60
basitali111 <allahbadshah148@gmail.com>
2023-05-01 23:32:21 +0500

    remove error files

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-
+echo '<h1 class="bg-blue-300">Hello world!</h1>';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,10 +8,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link href="styles.css" rel="stylesheet">
+   <link rel="stylesheet" href="/styles.css">
 </head>
 <body>
 <h1>Login Form</h1>
+<h1 class="text-3xl font-bold underline bg-blue-300">
+    Hello world!
+  </h1>
     <form>
       <label for="username">Username:</label>
       <input type="text" id="username" name="username" required><br>
