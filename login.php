25d10074c9738d18a983c2fbac155acc35d8bf3c
basitali111 <allahbadshah148@gmail.com>
2023-05-02 23:43:59 +0500

    Adding login button design

--- login.php
+++ login.php
@@ -1,6 +1,3 @@
-<?php
-echo '<h1 class="bg-blue-300">Hello world!</h1>';
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,21 +5,18 @@ echo '<h1 class="bg-blue-300">Hello world!</h1>';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-   <link rel="stylesheet" href="/styles.css">
+    <link rel="stylesheet"
+  href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+   <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<h1>Login Form</h1>
-<h1 class="text-3xl font-bold underline bg-blue-300">
-    Hello world!
-  </h1>
-    <form>
-      <label for="username">Username:</label>
-      <input type="text" id="username" name="username" required><br>
+<div class-="forms-container">
+  <div class=signin-signup>
+    <form action="" class="sign-in-form">
 
-      <label for="password">Password:</label>
-      <input type="password" id="password" name="password" required><br>
-    
-      <input type="submit" value="Submit">
     </form>
+  </div>
+</div>
+
 </body>
 </html>
